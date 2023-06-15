33ed2bb3babeb7a3dff4b983aabf4c755ef19371
smee6 <smee6@naver.com>
2023-06-15 15:57:38 +0900

    TS setting

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import "dotenv/config"; // env파일 사용
 import cors from "cors";
 import express from "express";
 import { User } from "./@types/index.d"; // @types/index에 있는 User 인터페이스 사용
+
+import UserRouter from './routes/userRouter.ts';
 const app = express();
 const PORT = process.env.PORT || 1234;
 
@@ -11,20 +13,21 @@ app.use(express.json());
 
 const router = express.Router();
 
-router.get("/api", (req, res, next) => {
-    res.send({ message: "welcome!" });
-});
 
 const user: User = {
     name: "jbee",
     id: 1,
 };
 
+
+app.use("/api/user", UserRouter);
+
 app.listen(PORT, () => {
     console.log(`
-  ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-  ┃   Server listening on port: ${PORT}    ┃
-  ┃     http://localhost:${PORT}/api       ┃
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+--------------------------------------------
+##$$$$ Server listening on port: ${PORT}
+http://localhost:${PORT}/test
+--------------------------------------------
+***********SERVER IS RUNNING NOW************
   `);
 });
